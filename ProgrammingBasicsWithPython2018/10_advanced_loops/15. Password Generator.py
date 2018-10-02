6d186e4ef32df7fb0a09d90321e1fa6a1f9d1d44
sasho <mushtty@gmail.com>
2018-10-01 21:10:02 -0400

    loops zadachi

--- ProgrammingBasicsWithPython2018/10_advanced_loops/15. Password Generator.py
+++ ProgrammingBasicsWithPython2018/10_advanced_loops/15. Password Generator.py
@@ -1,12 +1,8 @@
 n = int(input())
 l = int(input())
 for one in range(1, n+1):
-    print(one)
     for two in range(1, n+1):
-        print(two)
         for three in range(1, l+1):
-            print(chr(three + 96), end="")
             for four in range(1, l+1):
-                print(chr(four + 96), end="")
-                for five in range(max(one, two), n+1):
-                    print(five)
+                for five in range(max(one, two)+1, n+1):
+                    print(str(one) + str(two) + chr(three + 96) + chr(four + 96) + five, end=" ")
