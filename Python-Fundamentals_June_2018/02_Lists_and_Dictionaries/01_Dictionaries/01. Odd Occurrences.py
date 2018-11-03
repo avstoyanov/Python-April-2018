d06b111fd85c54d6efca827cef894b2c17ace837
sasho <mushtty@gmail.com>
2018-11-02 20:31:01 -0400

    python fundamentals

--- Python-Fundamentals_June_2018/02_Lists_and_Dictionaries/01_Dictionaries/01. Odd Occurrences.py
+++ Python-Fundamentals_June_2018/02_Lists_and_Dictionaries/01_Dictionaries/01. Odd Occurrences.py
@@ -1,2 +1,12 @@
 str_list = input().lower().split(" ")
-print(str_list)
+dict_str = {}
+for item in str_list:
+    if item in dict_str:
+        dict_str[item] += 1
+    else:
+        dict_str[item] = 1
+liststr = []
+for item in dict_str:
+    if dict_str[item]%2 != 0:
+        liststr.append(item)
+print(", ".join(liststr))
