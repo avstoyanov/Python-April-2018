29049a7d065186eda19f3cedb0438c66bb81fe03
grifdore <mushtty@gmail.com>
2019-11-10 13:44:35 -0500

    "social media posts" completed

--- Python-Fundamentals_June_2018/04_Objects_and_Classes/01. Distance Between Points.py
+++ Python-Fundamentals_June_2018/04_Objects_and_Classes/01. Distance Between Points.py
@@ -2,12 +2,12 @@
 class Point:
     def __init__(self, x, y):
         #assign objects
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
 
 
 # define distance function
-def CalcDistance(p1, p2):
+def calc_distance(p1, p2):
     a = p1.x - p2.x #define side 'a' for pythagoran distance calculation
     b = p1.y - p2.y #define side 'b' for pythagoran distance calculation
     return pow((pow(a, 2) + pow(b, 2)), 0.5) #find side 'c', or distance
@@ -22,8 +22,8 @@ x1, y1 = raw_p1.split(" ")
 x2, y2 = raw_p2.split(" ")
 
 # define points
-point1 = Point(int(x1), int(y1))
-point2 = Point(int(x2), int(y2))
+point1 = Point(x1, y1)
+point2 = Point(x2, y2)
 
 # get distance
-print(f"{CalcDistance(point1, point2):.3f}")
+print(f"{calc_distance(point1, point2):.3f}")
